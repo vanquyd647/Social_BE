8e85704ed48110b9bdf5d9702dcdfa9ccea56077
unknown <vanquyd647@gmail.com>
2024-12-15 23:30:27 +0700

    first commit build 1 15/12

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -61,6 +61,44 @@ const getMessagesInRoom = async (req, res) => {
     }
 };
 
+// const getMessagesInRoom = async (req, res) => {
+//     try {
+//         const { room_id } = req.params;
+//         const { limit = 20, cursor } = req.query;
+
+//         // Kiểm tra `room_id` hợp lệ
+//         if (!mongoose.Types.ObjectId.isValid(room_id)) {
+//             return res.status(400).json({ message: 'Invalid room ID format.' });
+//         }
+
+//         // Tạo điều kiện tìm kiếm
+//         const query = { room_id };
+
+//         if (cursor) {
+//             // Sử dụng `new` khi tạo ObjectId
+//             query._id = { $lt: new mongoose.Types.ObjectId(cursor) };
+//         }
+
+//         // Truy vấn các tin nhắn
+//         const messages = await Message.find(query)
+//             .sort({ createdAt: -1 })
+//             .limit(Number(limit))
+//             .exec();
+
+//         // Lấy nextCursor
+//         const nextCursor = messages.length > 0 ? messages[messages.length - 1]._id : null;
+
+//         res.status(200).json({
+//             message: 'Messages fetched successfully',
+//             messages,
+//             nextCursor,
+//         });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ message: 'Server Error', error: error.message });
+//     }
+// };
+
 module.exports = {
     sendMessage,
     getMessagesInRoom,
